90793e52f5cb6f5035733c3ccf6f8b18b486e112
jing.wang <admin@jingwangnet.com>
2018-12-04 13:01:14 +0800

    新文件：   js/main.js 	修改：     css/main.css 	修改：     index.html

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function navFunction() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+     x.style.display = "none";
+  } else {
+     x.style.display = "block";
+  }
+}
+
+
